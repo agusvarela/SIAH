af095c77843b94c7e6642ff0ab5871f40369d33d
agusvarela <agus.varela10@gmail.com>
2017-05-02 12:53:28 -0300

    ABM Localidades

---
+++ SIAH/Controllers/LocalidadesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SIAH.Context;
+using SIAH.Models;
+
+namespace SIAH.Controllers
+{
+    public class LocalidadesController : Controller
+    {
+        private SIAHContext db = new SIAHContext();
+
+        // GET: Localidades
+        public ActionResult Index()
+        {
+            return View(db.Localidades.ToList());
+        }
+
+        // GET: Localidades/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Localidad localidad = db.Localidades.Find(id);
+            if (localidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(localidad);
+        }
+
+        // GET: Localidades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Localidades/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre")] Localidad localidad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Localidades.Add(localidad);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(localidad);
+        }
+
+        // GET: Localidades/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Localidad localidad = db.Localidades.Find(id);
+            if (localidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(localidad);
+        }
+
+        // POST: Localidades/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre")] Localidad localidad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(localidad).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(localidad);
+        }
+
+        // GET: Localidades/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Localidad localidad = db.Localidades.Find(id);
+            if (localidad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(localidad);
+        }
+
+        // POST: Localidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Localidad localidad = db.Localidades.Find(id);
+            db.Localidades.Remove(localidad);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
