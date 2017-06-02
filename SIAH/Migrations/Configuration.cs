00b31296f63e0692889e62d8b491e232fac0721d
Facundo Suarez <suarezfacundon@gmail.com>
2017-06-02 11:50:01 -0300

    Publicacion

--- SIAH/Migrations/Configuration.cs
+++ SIAH/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace SIAH.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(SIAH.Context.SIAHContext context)
