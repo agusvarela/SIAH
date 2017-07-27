769030beb7e0030f0508373dc483d20ddb5a9d7d
gonzaalvarez70 <gonzaalvarez70@gmail.com>
2017-07-27 16:17:30 -0300

    Proyecto Unit Test agregado y vista del Reporte Consolidado

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIAH.Controllers;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    var controller = new PedidosController();
+
+        //    var result = controller.Details(28) as ViewResult;
+        //    Assert.AreEqual("Details", result.ViewName);
+        //}
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var controller = new PedidosController();
+
+            var result = controller.Create() as ViewResult;
+            Assert.AreEqual("Create", result.ViewName);
+        }
+    }
+}
