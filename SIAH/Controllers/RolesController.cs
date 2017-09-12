33aabbe7b57ddf46a0c29465e6e84184ff34a0b4
fagasro <fasgasro@gmail.com>
2017-09-12 16:06:27 -0300

    ABM Roles con ERRORES
    Al momento de dar de baja un rol ocurre una excepcion

---
+++ SIAH/Controllers/RolesController.cs
@@ -0,0 +1,122 @@
+﻿using SIAH.Context;
+using SIAH.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIAH.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: Roles
+        private SIAHContext db = new SIAHContext();
+        public ActionResult Index()
+        {         
+            return View(db.Roles.ToList());
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Roles.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Roles.Add(rol);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rol);
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Roles.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rol).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rol);
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Roles.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rol rol = db.Roles.Find(id);
+            db.Roles.Remove(rol);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
